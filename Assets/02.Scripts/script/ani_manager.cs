601115e67cca992310b9f130a699b760439e631b
Dory-noh <no6721@naver.com>
2025-02-25 19:08:10 +0900

    모드 설정 추가
    버튼 눌렀을 때 모드 설정되는 기능은 추가해야 한다.
    모드에 따라 플레이 종료 시점이 달라지도록 하였다.
    기본 플레이 모드 -> 21일 플레이
    endless 모드(기록 모드) -> 끝 없음

--- Assets/02.Scripts/script/ani_manager.cs
+++ Assets/02.Scripts/script/ani_manager.cs
@@ -8,11 +8,13 @@ public class ani_manager : MonoBehaviour
     public GameObject obj;
     public Text day_text;
     public int day;
+    public bool EndlessMode = false;
     // Start is called before the first frame update
     void Start()
     {
         day = 0;
-        day_text.text = "Day 1";
+        if(EndlessMode) day_text.text = "Day 1";
+        else day_text.text = "Day 1 / 21";
         StartCoroutine(sun_ani());
     }
 
@@ -25,28 +27,47 @@ public class ani_manager : MonoBehaviour
     {
         while (true)
         {
-            if ((day%21 == 0&&day!=0) || day > 1000)
+            if (EndlessMode)
             {
-                obj.transform.GetComponent<Animation>().Play();
-                day_text.text = $"<color=#ffff00>{day++ / 21} StageClear!</color>";
-                yield return new WaitForSeconds(1f);
-                day_text.text = "Day " + (day).ToString();
-                yield return new WaitForSeconds(19f);
-                obj.transform.GetComponent<Animation>().Rewind();
-                
-                if (day > 1000)
+                if ((day % 21 == 0 && day != 0) || day > 1000)
+                {
+                    obj.transform.GetComponent<Animation>().Play();
+                    day_text.text = $"<color=#ffff00>{day++ / 21} StageClear!</color>";
+                    yield return new WaitForSeconds(1f);
+                    day_text.text = "Day " + (day).ToString();
+                    yield return new WaitForSeconds(19f);
+                    obj.transform.GetComponent<Animation>().Rewind();
+
+                    if (day > 1000)
+                    {
+                        GameObject.FindGameObjectWithTag("ui_manager").GetComponent<ui_manager>().open_success();
+                        day_text.text = "성공~!";
+                        break;
+                    }
+                }
+                else
+                {
+                    obj.transform.GetComponent<Animation>().Play();
+                    day_text.text = "Day " + (++day).ToString();
+                    yield return new WaitForSeconds(20f);
+                    obj.transform.GetComponent<Animation>().Rewind();
+                }
+            }
+            else
+            {
+                if (day > 20)
                 {
                     GameObject.FindGameObjectWithTag("ui_manager").GetComponent<ui_manager>().open_success();
                     day_text.text = "성공~!";
                     break;
                 }
-            }
-            else
-            {
-                obj.transform.GetComponent<Animation>().Play();
-                day_text.text = "Day " + (++day).ToString();
-                yield return new WaitForSeconds(20f);
-                obj.transform.GetComponent<Animation>().Rewind();
+                else
+                {
+                    obj.transform.GetComponent<Animation>().Play();
+                    day_text.text = "Day " + (++day).ToString() + " / 21";
+                    yield return new WaitForSeconds(20f);
+                    obj.transform.GetComponent<Animation>().Rewind();
+                }
             }
         }
     }
